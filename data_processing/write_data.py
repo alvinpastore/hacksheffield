68ae1cd9395eb4600abc690eb9d627763fd0751e
Alvin <alvin.pastore@gmail.com>
2016-03-19 22:46:38 +0000

    added the print matrix function

--- data_processing/write_data.py
+++ data_processing/write_data.py
@@ -1,17 +1,17 @@
 import csv      # imports the csv module
 import datetime
 
+activity    = 'activity'
+time_       = 'time'
+duration    = 'duration'
+steps       = 'steps'
+distance    = 'distance'
+speed       = 'speed'
+bearing     = 'bearing'
+
 def write_hackdata(d):
 
     data_file = '../data/hackdata_post.csv'
-
-    activity    = 'activity'
-    time_       = 'time'
-    duration    = 'duration'
-    steps       = 'steps'
-    distance    = 'distance'
-    speed       = 'speed'
-    bearing     = 'bearing'
 
     header = ['index', activity, time_, duration, steps, distance, speed, bearing]
     outfile = open(data_file, 'wb') # opens the input csv file
@@ -25,6 +25,31 @@ def write_hackdata(d):
     finally:
         outfile.close()
 
+def map_activity(act):
+    if act == 'in_vehicle':
+        return 0
+    if act == 'on_bicycle':
+        return 1
+    if act == 'still':
+        return 2
+    if act == 'unknown':
+        return 3
+    if act == 'tilting':
+        return 4
+    if act == 'walking':
+        return 5
+    if act == 'running':
+        return 6
 
 
+def write_hackdata_matrix(d):
 
+    data_file = '../data/hackdata_matrix.csv'
+    outfile = open(data_file, 'wb') # opens the input csv file
+
+    try:
+        writer = csv.writer(outfile)
+        for row in d:
+            writer.writerow([row[time_].time().hour, row[duration], row[steps], row[distance], row[speed], row[bearing]])
+    finally:
+        outfile.close()
