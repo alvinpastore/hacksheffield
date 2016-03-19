7abdacea352e236dca8db678659ffa0dd07bd4f0
Alvin <alvin.pastore@gmail.com>
2016-03-19 19:15:10 +0000

    process data attempts to reassign undecided (unknown and tilting)

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -1,4 +1,5 @@
 from read_data import read_hackdata
+from write_data import write_hackdata
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,27 +24,26 @@ def print_data(d):
         print
 
 
-# removes rows with meaningless duration values (above 5/6 hours or negative durations)
-# removes rows with meaningless steps values (above 60k steps or negative steps)
-# removes rows with meaningless distance values (above 500km or negative distance)
-# removes rows with meaningless speed values (above 130km/h or negative speed)
-def clean_durations(d):
-
-    clean_data = []
+# removes rows with meaningless
+# duration  (above 5/6 hours or negative durations)
+# steps     (above 60k steps or negative steps)
+# distance  (above 500km or negative distance)
+# speed     (above 130km/h or negative speed)
+def clean_data(d):
+    clean_d = []
 
     for row in d:
         if 0 <= row[duration] < SIX_HOURS \
                 and 0 <= row[steps] < 60000\
                 and 0 <= row[distance] < 500\
                 and 0 <= row[speed] < 130:
-            clean_data.append(row)
+            clean_d.append(row)
 
-    return clean_data
+    return clean_d
 
 
 # criterion is a tuple (feature, value)
 def filter_data(d,criterion):
-
     filtered_data = []
 
     for row in d:
@@ -53,71 +53,61 @@ def filter_data(d,criterion):
     return filtered_data
 
 
+# won't work for activity or time_
 def get_stats(d,feature):
-    stats = {}
-
-    # extract the values for the criterion
+    s = {}
     feature_data = []
     for row in d:
-        feature_data.append(int(row[feature]))
+        feature_data.append(float(row[feature]))
 
-    stats['mean']     = np.mean(feature_data)
-    stats['std']      = np.std(feature_data)
-    stats['median']   = np.median(feature_data)
-    stats['raw_data'] = feature_data
+    s['mean']     = np.mean(feature_data)
+    s['std']      = np.std(feature_data)
+    s['median']   = np.median(feature_data)
+    s['raw_data'] = feature_data
 
-    return stats
+    return s
 
 
-def translate_unknown(d):
+def translate_undecided(d):
+    translate_amount = 0
 
-    for i in range(2,len(d)-2):
-        # previous two entries are the same of next two but the middle one is different
-        previous_match = d[i-2][activity] == d[i-1][activity]
-        next_match = d[i+1][activity] == d[i+2][activity]
-        jump_match = d[i-1][activity] == d[i+1][activity] and next_match and previous_match
-        d[i][activity] == ''
-        if d[i][activity] != jump_match:
-            print d[i-2]
-            print d[i-1]
-            print d[i]
-            print d[i+1]
-            print d[i+2]
-
+    for i in range(1,len(d)-1):
+        undecided = d[i][activity] == 'unknown' or d[i][activity] == 'tilting'
+        # if the previous and next activities are the same
+        if d[i-1][activity] == d[i+1][activity] and d[i][activity] != d[i+1][activity] and undecided:
+            translate_amount += 1
             d[i][activity] = d[i-1][activity]
+        # if the previous and next activities are not the same use the time difference to translate unknown
+        elif  d[i-1][activity] != d[i+1][activity] and d[i][activity] != d[i+1][activity] and undecided:
+            translate_amount += 1
+            t_prev = d[i-1][time_]
+            t_next = d[i+1][time_]
+            if d[i][time_] - t_prev > t_next - d[i][time_]:
+                d[i][activity] = d[i+1][activity]
+            else:
+                d[i][activity] = d[i-1][activity]
 
-
+    print "translated " + str(translate_amount) + " rows"
     return d
 
+# step 1 read the data
 hackdata = read_hackdata()
 original_data_amount = len(hackdata)
-hackdata = clean_durations(hackdata)
-print "pruned " + str(original_data_amount - len(hackdata)) + " rows"
 
-stats = get_stats(hackdata,duration)
+# step 2 clean the data from unrealistic values
+hackdata = clean_data(hackdata)
+clean_data_amount = len(hackdata)
 
-print stats['mean']
-print stats['median']
-print stats['std']
+# step 3 translate unknown and itlting to activity
+hackdata = translate_undecided(hackdata)
 
-plt.hist(stats['raw_data'], bins=100)
-plt.show()
+distance_stats = get_stats(hackdata,distance)
 
+print 'distance stats \n' \
+      'mean: {0},  median: {1} std: {2} max: {3}'.\
+    format(distance_stats['mean'],distance_stats['median'],distance_stats['std'],max(distance_stats['raw_data']))
 
-#
-# walking_data    = filter_data(hackdata,(activity,'walking'))
-# still_data      = filter_data(hackdata,(activity,'still'))
-# unknown_data    = filter_data(hackdata,(activity,'unknown'))
-# in_vehicle_data = filter_data(hackdata,(activity,'in_vehicle'))
-# on_foot_data    = filter_data(hackdata,(activity,'on_foot_vehicle'))
-# on_bicycle_data = filter_data(hackdata,(activity,'on_bicycle'))
-# tilting_data    = filter_data(hackdata,(activity,'tilting'))
-#
-#
-# full_stats       = get_stats(hackdata,duration)
-# walking_stats    = get_stats(walking_data,duration)
-# still_stats      = get_stats(still_data,duration)
-# unknown_stats    = get_stats(unknown_data,duration)
-# in_vehicle_stats = get_stats(in_vehicle_data,duration)
-# on_bicycle_stats = get_stats(on_bicycle_data,duration)
-# tilting_stats    = get_stats(tilting_data,duration)
+#plt.hist(distance_stats['raw_data'], bins=100)
+#plt.show()
+
+#write_hackdata(hackdata)
