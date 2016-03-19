fdc80a627c63727a35a72adf6008624c760ff131
Alvin <alvin.pastore@gmail.com>
2016-03-19 17:57:05 +0000

    cleaning crazy values of steps, distance, speed, duration

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -23,36 +23,42 @@ def print_data(d):
         print
 
 
-# removes rows with meaningless values (crazy-high [above 5 hours] or negative time)
+# removes rows with meaningless duration values (above 5/6 hours or negative durations)
+# removes rows with meaningless steps values (above 60k steps or negative steps)
+# removes rows with meaningless distance values (above 500km or negative distance)
+# removes rows with meaningless speed values (above 130km/h or negative speed)
 def clean_durations(d):
 
     clean_data = []
 
     for row in d:
-        if 0 <= row[duration] < SIX_HOURS:
+        if 0 <= row[duration] < SIX_HOURS \
+                and 0 <= row[steps] < 60000\
+                and 0 <= row[distance] < 500\
+                and 0 <= row[speed] < 130:
             clean_data.append(row)
 
     return clean_data
 
 
 # criterion is a tuple (feature, value)
-def filter_data(data,criterion):
+def filter_data(d,criterion):
 
     filtered_data = []
 
-    for row in data:
+    for row in d:
         if row[criterion[0]].lower() == criterion[1]:
             filtered_data.append(row)
 
     return filtered_data
 
 
-def get_stats(data,feature):
+def get_stats(d,feature):
     stats = {}
 
     # extract the values for the criterion
     feature_data = []
-    for row in data:
+    for row in d:
         feature_data.append(int(row[feature]))
 
     stats['mean']     = np.mean(feature_data)
@@ -62,8 +68,32 @@ def get_stats(data,feature):
 
     return stats
 
+
+def translate_unknown(d):
+
+    for i in range(2,len(d)-2):
+        # previous two entries are the same of next two but the middle one is different
+        previous_match = d[i-2][activity] == d[i-1][activity]
+        next_match = d[i+1][activity] == d[i+2][activity]
+        jump_match = d[i-1][activity] == d[i+1][activity] and next_match and previous_match
+        d[i][activity] == ''
+        if d[i][activity] != jump_match:
+            print d[i-2]
+            print d[i-1]
+            print d[i]
+            print d[i+1]
+            print d[i+2]
+
+            d[i][activity] = d[i-1][activity]
+
+
+    return d
+
 hackdata = read_hackdata()
+original_data_amount = len(hackdata)
 hackdata = clean_durations(hackdata)
+print "pruned " + str(original_data_amount - len(hackdata)) + " rows"
+
 stats = get_stats(hackdata,duration)
 
 print stats['mean']
